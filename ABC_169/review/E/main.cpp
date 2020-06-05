8bbd7372bbe14b6cfac768331f41db25c934edee
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-06-06 03:03:40 +0900

    now reviewing

--- ABC_169/review/E/main.cpp
+++ ABC_169/review/E/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<int> ls(N), rs(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> ls[i] >> rs[i];
+    }
+    sort(ls.begin(), ls.end());
+    sort(rs.begin(), rs.end());
+    int ans = 0;
+    if (N%2 == 1) {
+        ans = rs[N/2] - ls[N/2] + 1;
+    }
+    else {
+        ans = rs[N/2-1] + rs[N/2] - ls[N/2-1] - ls[N/2] + 1;
+    }
+    cout << ans << endl;
     return 0;
 }
