d9c7bfa6c9d0d29481a6b321fcbf5c26139473a5
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-06-26 14:54:30 +0900

    Jun. 26th, 2020

---
+++ p_ABC_164/A/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#define rep(i,n) for (int i = 0; i < n; ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+  int S, W;
+  cin >> S >> W;
+  if (S > W) cout << "safe" << endl;
+  else cout << "unsafe" << endl;
+  return 0;
+}
