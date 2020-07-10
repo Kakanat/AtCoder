716fa76b8fcdb0966a370aadc6f767b904b8abbd
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-10 14:21:35 +0900

    Jul. 10th, 2020

--- p_ABC_151/B/main.cpp
+++ p_ABC_151/B/main.cpp
@@ -11,5 +11,16 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N, M, K;
+  cin >> N >> K >> M;
+  int sum = 0;
+  rep(i,N-1) {
+    int t = 0;
+    cin >> t;
+    sum += t;
+  }
+  if (M * N - sum <= K) cout << max(0, M * N - sum) << endl;
+  else cout << -1 << endl;
+
   return 0;
 }
--- p_ABC_151/C/main.cpp
+++ p_ABC_151/C/main.cpp
@@ -11,5 +11,30 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> p(M);
+  vector<string> S(M);
+  rep(i,M) cin >> p[i] >> S[i];
+  vector<bool> result(N);
+  vector<int> wa(N);
+  rep(i,M) {
+    if (!result[p[i]-1]) {
+      if (S[i] == "AC") {
+        result[p[i]-1] = true;
+      }
+      else {
+        ++wa[p[i]-1];
+      }
+    }
+  }
+  int ans = 0, pena = 0;
+  rep(i,N) {
+    if (result[i]) {
+      ++ans;
+      pena += wa[i];
+    }
+  }
+  cout << ans << " " << pena << endl;
   return 0;
 }
