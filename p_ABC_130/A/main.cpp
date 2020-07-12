76b03f5fb80b06e49a4a4460af82fd77d6b02d59
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-12 09:33:51 +0900

    Jul. 12th, 2020

---
+++ p_ABC_130/A/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#define rep(i,n) for (int i = 0; i < n; ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+  int X, A;
+  cin >> X >> A;
+  if (X < A) cout << 0 << endl;
+  else cout << 10 << endl;
+  return 0;
+}
