9b4a8ef33d412c1ffd539d46b73ddfca02acb5dd
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-12 10:46:34 +0900

    Jul. 12th, 2020

--- p_ABC_150/B/main.cpp
+++ p_ABC_150/B/main.cpp
@@ -11,5 +11,14 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N;
+  cin >> N;
+  string S;
+  cin >> S;
+  int ans = 0;
+  rep(i,N-2) {
+    if (S[i] == 'A' && S[i+1] == 'B' && S[i+2] == 'C') ++ans;
+  }
+  cout << ans << endl;
   return 0;
 }
--- p_ABC_150/C/main.cpp
+++ p_ABC_150/C/main.cpp
@@ -11,5 +11,19 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N), B(N);
+  rep(i,N) cin >> A[i];
+  rep(i,N) cin >> B[i];
+  vector<int> p(N);
+  rep(i,N) p[i] = i+1;
+
+  map<vector<int>, int> mp;
+  do {
+    mp[p] = mp.size();
+  } while (next_permutation(p.begin(), p.end()));
+  int ans = abs(mp[A] - mp[B]);
+  cout << ans << endl;
   return 0;
 }
