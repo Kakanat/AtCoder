78082f5b0921db43bdf57c32fe0ab6ae39871977
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-14 11:36:38 +0900

    Jul. 14th, 2020

--- p_ABC_147/B/main.cpp
+++ p_ABC_147/B/main.cpp
@@ -11,5 +11,12 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  string S;
+  cin >> S;
+  int cnt = 0;
+  rep(i,S.size()/2) {
+    if (S[i] != S[S.size()-1-i]) ++cnt;
+  }
+  cout << cnt << endl;
   return 0;
 }
