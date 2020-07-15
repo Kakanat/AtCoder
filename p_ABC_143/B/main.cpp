8a889b8d8c434d30597f9cd950bc00a25f2b07f0
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-16 07:08:14 +0900

    Jul. 16th, 2020

--- p_ABC_143/B/main.cpp
+++ p_ABC_143/B/main.cpp
@@ -11,5 +11,16 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<int> d(N);
+  rep(i,N) cin >> d[i];
+  int sum = 0;
+  for (int i = 0; i < N - 1; ++i) {
+    for (int j = i + 1; j < N;  ++j) {
+      sum += d[i] * d[j];
+    }
+  }
+  cout << sum << endl;
   return 0;
 }
