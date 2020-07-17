20c930530b23d8af414823409a63fa04d5b453ab
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-18 01:24:12 +0900

    Jul. 18th, 2020

--- p_ABC_140/C/main.cpp
+++ p_ABC_140/C/main.cpp
@@ -11,5 +11,18 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<int> B(N-1);
+  rep(i,N-1) cin >> B[i];
+  vector<int> A(N);
+  A[0] = B[0];
+  A[N-1] = B[N-2];
+  for (int i = 1; i < N-1; ++i) {
+    A[i] = min(B[i-1], B[i]);
+  }
+  int ans = 0;
+  rep(i,N) ans += A[i];
+  cout << ans << endl;
   return 0;
 }
