c141dc85d06e20593ed838bad963022b71e16c85
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-17 12:47:38 +0900

    Jul. 17th, 2020

--- p_ABC_147/C/main.cpp
+++ p_ABC_147/C/main.cpp
@@ -10,6 +10,36 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
+int x[15][15];
+int y[15][15];
+
 int main() {
+  int N; cin >> N;
+  vector<int> A(N);
+  rep(i,N) {
+    cin >> A[i];
+    rep(j,A[i]) {
+      cin >> x[i][j] >> y[i][j];
+    }
+  }
+  int ans = 0;
+  rep(i,1<<N) { // 1 is honest, 0 is unkind.
+    bool ok = true;
+    int honest = 0;
+    rep(j,N) {
+      if (i>>j&1) { // if j is honest.
+        rep(k,A[j]) {
+          if (y[j][k] != (i>>(x[j][k]-1)&1)) {
+            ok = false;
+            break;
+          }
+        }
+      if (ok) ++honest;
+      else break;
+      }
+    }
+    if (ok) ans = max(ans, honest);
+  }
+  cout << ans << endl;
   return 0;
 }
