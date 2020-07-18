502557eab493617caae9c3ce7fe41577a724ae10
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-18 16:05:11 +0900

    Jul. 18th, 2020

--- p_ABC_146/C/main.cpp
+++ p_ABC_146/C/main.cpp
@@ -10,6 +10,36 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
+int d(int N) {
+  int t = 0;
+  while (N > 0) {
+    N /= 10;
+    ++t;
+  }
+  return t;
+}
+
 int main() {
+  ll A, B;
+  ll X;
+  cin >> A >> B >> X;
+  if (A + B > X) {
+    cout << 0 << endl;
+    return 0;
+  }
+  int l = 1, r = 1000000000;
+  while (l < r) {
+    // cout << l << r << endl;
+    int t = (l+r+1)/2;
+    if (A*t + B*d(t) > X) {
+      r = t - 1;
+    }
+    else {
+      l = t;
+    }
+    // cout << l << r << endl;
+    // return 0;
+  }
+  cout << l << endl;
   return 0;
 }
