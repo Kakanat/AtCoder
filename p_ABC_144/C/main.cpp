76693aaac353035ef4ce2bedd9cd3050ef7308b4
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-20 02:00:57 +0900

    Jul. 20th, 2020

--- p_ABC_144/C/main.cpp
+++ p_ABC_144/C/main.cpp
@@ -11,5 +11,16 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  ll N;
+  cin >> N;
+  int M = 1000000;
+  ll ans = 1000000000010;
+  for (int i = 1; i <= M; ++i) {
+    if (N%i==0) {
+      ll temp = i + N/i;
+      ans = min(ans, temp);
+    }
+  }
+  cout << ans - 2 << endl;
   return 0;
 }
