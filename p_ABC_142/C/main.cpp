9d61393a4be2eeaa04ab121c4db677c70f253df2
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-20 10:23:54 +0900

    Jul. 20th, 2020

--- p_ABC_142/C/main.cpp
+++ p_ABC_142/C/main.cpp
@@ -11,5 +11,15 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<P> p(N);
+  rep(i,N) {
+    p[i].second = i + 1;
+    cin >> p[i].first;
+  }
+  sort(p.begin(), p.end());
+  rep(i,N) cout << p[i].second << " ";
+  cout << endl;
   return 0;
 }
--- p_ABC_143/C/main.cpp
+++ p_ABC_143/C/main.cpp
@@ -11,5 +11,16 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N;
+  cin >> N;
+  string S;
+  cin >> S;
+  int ans = 1;
+  char temp = S[0];
+  for (int i = 1; i < N; ++i) {
+    if (temp != S[i]) ++ans;
+    temp = S[i];
+  }
+  cout << ans << endl;
   return 0;
 }
