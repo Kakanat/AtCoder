0081126965ea19878a6f5b4bf79a20ae4bab5448
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-23 12:06:33 +0900

    Jul. 23rd, 2020

--- p_ABC_133/B/main.cpp
+++ p_ABC_133/B/main.cpp
@@ -10,6 +10,21 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
+int X[10][10];
+
 int main() {
+  int N, D;
+  cin >> N >> D;
+  rep(i,N) rep(j,D) cin >> X[i][j];
+  int ans = 0;
+  for (int i = 0; i < N; ++i) {
+    for (int j = i+1; j < N; ++j) {
+      int d = 0;
+      rep(k,D) d += (X[i][k] - X[j][k]) * (X[i][k] - X[j][k]);
+      int t = sqrt(d);
+      if (t*t == d) ++ans;
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
--- p_ABC_133/C/main.cpp
+++ p_ABC_133/C/main.cpp
@@ -11,5 +11,24 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  ll L, R;
+  cin >> L >> R;
+  if (R - L >= 2019) {
+    cout << 0 << endl;
+    return 0;
+  }
+  int l = L % 2019, r = R % 2019;
+  if (l >= r) {
+    cout << 0 << endl;
+    return 0;
+  }
+  int ans = 2019;
+  for (int i = l; i <= r; ++i) {
+    for (int j = i+1; j <= r; ++j) {
+      int temp = (i * j) % 2019;
+      ans = min(ans, temp);
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
