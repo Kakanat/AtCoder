5a363cbabbfbc5cc9ab090feeb10ef699af759eb
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-24 03:28:22 +0900

    Jul. 24th, 2020

--- p_ABC_132/B/main.cpp
+++ p_ABC_132/B/main.cpp
@@ -11,5 +11,14 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int n;
+  cin >> n;
+  vector<int> p(n);
+  rep(i,n) cin >> p[i];
+  int ans = 0;
+  for (int i = 1; i < n-1; ++i) {
+    if ((p[i-1] < p[i] && p[i] < p[i+1]) || (p[i-1] > p[i] && p[i] > p[i+1])) ++ans;
+  }
+  cout << ans << endl;
   return 0;
 }
--- p_ABC_132/C/main.cpp
+++ p_ABC_132/C/main.cpp
@@ -11,5 +11,12 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<int> d(N);
+  rep(i,N) cin >> d[i];
+  sort(d.begin(), d.end());
+  int ans = d[N/2] - d[N/2 - 1];
+  cout << ans << endl;
   return 0;
 }
