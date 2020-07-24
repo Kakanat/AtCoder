e38d691b35178166a5ac1fdcde3f2b14bfd6e3d4
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-25 00:50:20 +0900

    Jul. 25th, 2020

--- p_ABC_131/B/main.cpp
+++ p_ABC_131/B/main.cpp
@@ -11,5 +11,16 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N, L;
+  cin >> N >> L;
+  int sum = 0;
+  int eat = 9999999;
+  rep(i,N) {
+    int temp = L + i;
+    if (abs(temp) < abs(eat)) eat = temp;
+    sum += temp;
+  }
+  int ans = sum - eat;
+  cout << ans << endl;
   return 0;
 }
