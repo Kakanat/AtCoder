d76024a2eaebeab5673a86ef05b23c8fbb732ffb
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-28 01:47:49 +0900

    Jul. 28th, 2020

--- p_ABC_129/B/main.cpp
+++ p_ABC_129/B/main.cpp
@@ -11,5 +11,21 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<int> W(N);
+  int sum = 0;
+  rep(i,N) {
+    cin >> W[i];
+    sum += W[i];
+  }
+  int temp_sum = 0;
+  int ans = 999999;
+  rep(i,N) {
+    temp_sum += W[i];
+    sum -= W[i];
+    ans = min(ans, abs(temp_sum - sum));
+  }
+  cout << ans << endl;
   return 0;
 }
