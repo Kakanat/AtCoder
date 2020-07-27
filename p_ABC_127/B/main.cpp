eae1a8f047f8204cc91ee2579b2ecf524b74bbbc
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-27 10:21:52 +0900

    Jul. 27th, 2020

--- p_ABC_127/B/main.cpp
+++ p_ABC_127/B/main.cpp
@@ -11,5 +11,11 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int r, d, x;
+  cin >> r >> d >> x;
+  for (int i = 1; i <= 10; ++i) {
+    x = r * x - d;
+    cout << x << endl;
+  }
   return 0;
 }
--- p_ABC_127/C/main.cpp
+++ p_ABC_127/C/main.cpp
@@ -11,5 +11,15 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N, M;
+  cin >> N >> M;
+  int l = 1, r = N;
+  for (int i = 1; i <= M; ++i) {
+    int a, b;
+    cin >> a >> b;
+    l = max(l, a);
+    r = min(r, b);
+  }
+  cout << max(0, r-l+1) << endl;
   return 0;
 }
