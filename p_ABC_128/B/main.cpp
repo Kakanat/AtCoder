0aef42fd1638436abec298efd35bfd04be46c891
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-07-29 02:11:43 +0900

    Jul. 29th, 2020

--- p_ABC_128/B/main.cpp
+++ p_ABC_128/B/main.cpp
@@ -10,6 +10,27 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
+
+bool compare_by_b(pair<int, pair<string, int>> a, pair<int, pair<string, int>> b) {
+  if(a.second.first != b.second.first){
+      return a.second.first < b.second.first;
+  }else{
+      return a.second.second < b.second.second;
+  }
+}
+
 int main() {
+  int N;
+  cin >> N;
+  vector<pair<int, pair<string, int>>> restaurant(N);
+  rep(i,N) {
+    restaurant[i].first = i+1;
+    cin >> restaurant[i].second.first >> restaurant[i].second.second;
+    restaurant[i].second.second *= -1;
+  }
+  sort(restaurant.begin(), restaurant.end(), compare_by_b);
+  rep(i,N) {
+    cout << restaurant[i].first << endl;
+  }
   return 0;
 }
