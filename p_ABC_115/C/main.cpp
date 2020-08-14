ff744311f98a0f49417bd6b6ff81e7e3d5823d4b
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-08-14 11:53:19 +0900

    Aug. 14th, 2020

--- p_ABC_115/C/main.cpp
+++ p_ABC_115/C/main.cpp
@@ -11,5 +11,15 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N, K; cin >> N >> K;
+  vector<ll> h(N);
+  rep(i,N) cin >> h[i];
+  sort(h.begin(), h.end(), greater<ll>());
+  ll min_dif = 1000000010;
+  rep(i,N-K+1) {
+    ll temp_dif = h[i] - h[i+K-1];
+    min_dif = min(min_dif, temp_dif);
+  }
+  cout << min_dif << endl;
   return 0;
 }
