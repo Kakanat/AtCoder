13957ea201a5ea2d8047219fef3935b1864f60c3
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-08-15 14:10:12 +0900

    Aug. 15th, 2020

--- p_ABC_124/C/main.cpp
+++ p_ABC_124/C/main.cpp
@@ -11,5 +11,15 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  string S; cin >> S;
+  int ans1 = 0, ans2 = 0;
+  rep(i,S.size()) {
+    if (i%2==0 && S[i]=='0') ++ans1;
+    if (i%2==0 && S[i]=='1') ++ans2;
+    if (i%2==1 && S[i]=='1') ++ans1;
+    if (i%2==1 && S[i]=='0') ++ans2;
+  }
+  cout << min(ans1, ans2) << endl;
+
   return 0;
 }
