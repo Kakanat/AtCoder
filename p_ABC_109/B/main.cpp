b8d2ea5fcbe4ea40c7ff9e59c7918ca7c552e36d
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-08-20 02:36:49 +0900

    Aug. 20th, 2020

--- p_ABC_109/B/main.cpp
+++ p_ABC_109/B/main.cpp
@@ -12,5 +12,18 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int N; cin >> N;
+  map<string, int> m;
+  char last;
+  bool ok = true;
+  rep(i,N) {
+    string w; cin >> w;
+    if (i > 0 && w[0] != last) ok = false;
+    last = w.back();
+    m[w]++;
+    if (m[w] > 1) ok = false; 
+  }
+  if (ok) cout << "Yes" << endl;
+  else cout << "No" << endl;
   return 0;
 }
