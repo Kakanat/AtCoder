98f1c1f97dd67065a159170df8a0bab91761048b
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-08-27 09:42:29 +0900

    Aug. 27th, 2020

--- p_ABC_120/C/main.cpp
+++ p_ABC_120/C/main.cpp
@@ -11,5 +11,11 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
-  return 0;
+  string S; cin >> S;
+  int zero = 0, one = 0;
+  for (auto i : S) {
+    if (i == '0') ++zero;
+    else ++one;
+  }
+  cout << 2 * min(zero, one) << endl;
 }
