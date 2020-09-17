74f3a5b5a254d543759154a43dee8a462794410e
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-09-17 10:09:02 +0900

    Sep. 17th, 2020

---
+++ p_ABC_087/A/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#define rep(i,n) for (int i = 0; i < n; ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+  int x, a, b; cin >> x >> a >> b;
+  x -= a;
+  cout << x % b << endl;
+  return 0;
+}
